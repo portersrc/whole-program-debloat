4cfa6820a2b6c8926509daa1fd9f8a8ea4938124
rudy jantz <porter@gatech.edu>
2020-11-16 08:00:26 -0500

    parse debprof logs together to capture proper column count and func set ids

--- src/learning/parse_debprof_out.py
+++ src/learning/parse_debprof_out.py
@@ -17,22 +17,15 @@ import sys
 # will probably be done eventually.
 #
 
-if len(sys.argv) != 3:
-    print('check args. need to supply a filename prefix (train or test) ' \
-          'and max-num-args (see debprof_pass_stats.txt)')
-    sys.exit(1)
 
-FILENAME_PREFIX = sys.argv[1]
-if FILENAME_PREFIX not in {'train', 'test'}:
-    print('check args. filename prefix should be train or test')
-    sys.exit(1)
 
-MAX_NUM_ARGS = int(sys.argv[2])
 
-DEBPROF_INPUT_FILENAME = FILENAME_PREFIX + '-debprof.out'
-DEBPROF_OUTPUT_FILENAME = 'final-' + DEBPROF_INPUT_FILENAME
+TRAIN_DEBPROF_INPUT_FILENAME  = 'train-debprof.out'
+TEST_DEBPROF_INPUT_FILENAME   = 'test-debprof.out'
+TRAIN_DEBPROF_OUTPUT_FILENAME = 'final-' + TRAIN_DEBPROF_INPUT_FILENAME
+TEST_DEBPROF_OUTPUT_FILENAME  = 'final-' + TEST_DEBPROF_INPUT_FILENAME
 
-FUNC_SET_IDS_FILENAME = FILENAME_PREFIX + '-func-set-ids-to-funcs.out'
+FUNC_SET_IDS_FILENAME = 'debprof-func-set-ids-to-funcs.out'
 
 DEBPROF_COLUMN_HEADERS = ''
 
@@ -40,12 +33,16 @@ DEBPROF_COLUMN_HEADERS = ''
 
 PREDICTED_FUNCS_SET_SIZE = 5
 
+max_final_columns = 0
+
+
 # Indices of the fields from the input file.
 # These shift up by 1 in our output file.
 CALLSITE_ID_IDX    = 0
 CALLED_FUNC_ID_IDX = 1
 
 num_primed_lines = 0
+
 
 curr_line_to_write = ''
 line_buf = [None] * PREDICTED_FUNCS_SET_SIZE
@@ -64,6 +61,19 @@ buf_called_func_ids = [''] * PREDICTED_FUNCS_SET_SIZE
 # '110').
 called_funcs_to_id   = {}
 called_funcs_counter = 0
+
+
+def count_max_columns(filename):
+    print('Checking {} for max columns'.format(filename))
+    max_cols = 0
+    with open(filename) as f:
+        _ = f.readline() # skip column headers
+        for line in f:
+            c = line.count(',') + 1 # 1 comma -> 2 cols. 2 commas -> 3 cols, etc
+            if c > max_cols:
+                max_cols = c
+    return max_cols
+
 
 
 
@@ -91,7 +101,31 @@ def prime_line_buf(f):
             update_line_buf(line)
 
 
-def empty_line_buf():
+def get_func_set_id():
+    global called_funcs_counter
+    global called_funcs_to_id
+    called_funcs = ''
+    tmp = ''
+    for func_id in sorted(buf_called_func_ids):
+        if func_id and func_id != tmp:
+            called_funcs += func_id + ','
+            tmp = func_id
+    if called_funcs == '':
+        return -1
+    if called_funcs not in called_funcs_to_id:
+        called_funcs_to_id[called_funcs] = called_funcs_counter
+        called_funcs_counter += 1
+    return called_funcs_to_id[called_funcs]
+
+
+def get_num_columns_to_pad():
+    # Each line is comma-separated, and there's no trailing comma at the end
+    # of the line. So the number of columns is count(',') + 1
+    num_args = curr_line_to_write.count(',') + 1
+    return max_final_columns - num_args
+
+
+def empty_line_buf(fp_out):
     global curr_line_to_write
     global line_buf
     global buf_idx 
@@ -114,61 +148,54 @@ def empty_line_buf():
         # was already called multiple times, it's better to just train on
         # the other instances that actually led to future calls.
         if func_set_id >= 0:
-            num_args = len(curr_line_to_write.split(','))
-            columns_to_pad = MAX_NUM_ARGS - num_args
-            for i in range(columns_to_pad):
+            for i in range(get_num_columns_to_pad()):
                 curr_line_to_write = curr_line_to_write+',0'
             line_to_write = '{},{}\n'.format(func_set_id, curr_line_to_write)
             fp_out.write(line_to_write)
 
 
+def post_process(input_filename, fp_out):
+    global curr_line_to_write
+    with open(input_filename) as f:
+        prime_line_buf(f)
+        fp_out.write('predicted_func_set_id,{}'.format(DEBPROF_COLUMN_HEADERS))
+        for line in f:
+            line = line.strip()
+            update_line_buf(line)
+            func_set_id = get_func_set_id()
+            for i in range(get_num_columns_to_pad()):
+                curr_line_to_write = curr_line_to_write+',0'
+            line_to_write = '{},{}\n'.format(func_set_id, curr_line_to_write)
+            fp_out.write(line_to_write)
+        empty_line_buf(fp_out)
 
 
-def get_func_set_id():
-    global called_funcs_counter
-    global called_funcs_to_id
-    called_funcs = ''
-    tmp = ''
-    for func_id in sorted(buf_called_func_ids):
-        if func_id and func_id != tmp:
-            called_funcs += func_id + ','
-            tmp = func_id
-    if called_funcs == '':
-        return -1
-    if called_funcs not in called_funcs_to_id:
-        called_funcs_to_id[called_funcs] = called_funcs_counter
-        called_funcs_counter += 1
-    return called_funcs_to_id[called_funcs]
+def get_max_final_columns():
+    a = count_max_columns(TRAIN_DEBPROF_INPUT_FILENAME)
+    b = count_max_columns(TEST_DEBPROF_INPUT_FILENAME)
+    # final-*-debprof.out will have a prefixed target value on each row,
+    # so it will have 1 more column than the max of the train and test runs.
+    return max(a, b) + 1
 
+max_final_columns = get_max_final_columns()
 
-
-
-fp_out = open(DEBPROF_OUTPUT_FILENAME, 'w')
+fp_train_out = open(TRAIN_DEBPROF_OUTPUT_FILENAME, 'w')
+fp_test_out  = open(TEST_DEBPROF_OUTPUT_FILENAME, 'w')
 fp_func_set_ids = open(FUNC_SET_IDS_FILENAME, 'w')
 
 
 print()
-print('input log: {}'.format(DEBPROF_INPUT_FILENAME))
-print('MAX_NUM_ARGS = {}'.format(MAX_NUM_ARGS))
+print('input logs:')
+print('  {}'.format(TRAIN_DEBPROF_INPUT_FILENAME))
+print('  {}'.format(TEST_DEBPROF_INPUT_FILENAME))
+print('max_final_columns = {}'.format(max_final_columns))
 print('PREDICTED_FUNCS_SET_SIZE = {}'.format(PREDICTED_FUNCS_SET_SIZE))
 print()
 print('Starting...')
 
 
-with open(DEBPROF_INPUT_FILENAME) as f:
-    prime_line_buf(f)
-    fp_out.write('predicted_func_set_id,{}'.format(DEBPROF_COLUMN_HEADERS))
-    for line in f:
-        line = line.strip()
-        update_line_buf(line)
-        func_set_id = get_func_set_id()
-        num_args = len(curr_line_to_write.split(','))
-        columns_to_pad = MAX_NUM_ARGS - num_args
-        for i in range(columns_to_pad):
-            curr_line_to_write = curr_line_to_write+',0'
-        line_to_write = '{},{}\n'.format(func_set_id, curr_line_to_write)
-        fp_out.write(line_to_write)
-    empty_line_buf()
+post_process(TRAIN_DEBPROF_INPUT_FILENAME, fp_train_out)
+post_process(TEST_DEBPROF_INPUT_FILENAME, fp_test_out)
 
 
 fp_func_set_ids.write('predicted_func_set_id: called_func_id1,called_func_id2,...\n')
@@ -177,13 +204,14 @@ for called_funcs in called_funcs_to_id:
                                             called_funcs))
                                             
     
-fp_out.close()
+fp_train_out.close()
+fp_test_out.close()
 fp_func_set_ids.close()
 
 print('...Done')
 print()
-print('See {} for output log (prefixed predicted-func-set-id on each line, '
-      'and uniform column lengths over all rows)'.format(DEBPROF_OUTPUT_FILENAME))
+print('See {} and {} for output log (prefixed predicted-func-set-id on each line, '
+      'and uniform column lengths over all rows)'.format(TRAIN_DEBPROF_OUTPUT_FILENAME, TEST_DEBPROF_OUTPUT_FILENAME))
 print()
 print('See {} for the mapping of each predicted func set ID to the func IDs '
       'within it'.format(FUNC_SET_IDS_FILENAME))
