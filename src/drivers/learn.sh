1ac0382e25acc475845e9d76e58e70d240503f3b
rudy jantz <porter@gatech.edu>
2021-01-05 21:45:34 -0500

    invoke learn python directly

--- src/drivers/learn.sh
+++ src/drivers/learn.sh
@@ -11,16 +11,22 @@ for BMARK in "${BMARKS[@]}"; do
 
     CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-debprof.out
     TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-test-debprof.out
+    #CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-debprof.outsmall
+    #TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-test-debprof.outsmall
+    #CSV_FILENAME=${BASE_PATH}/${FOLDER}/train-debprof.outsmall
+    #TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/test-debprof.outsmall
+    #CSV_FILENAME=${BASE_PATH}/${FOLDER}/train-debprof.outfull
+    #TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/test-debprof.outfull
     SAVE_PLOTS=${BMARK}-dt
     OUTPUT_FILE=${BMARK}-accuracy.txt
 
     mkdir -p $BMARK
     pushd $BMARK &> /dev/null
-    python ${LEARN_DT_PY} \
-          -csv_file_name ${CSV_FILENAME} \
-          -test_csv_file_name ${TEST_CSV_FILENAME} \
-          -save_plots ${SAVE_PLOTS} \
-          &> ${OUTPUT_FILE}
+    ${LEARN_PY} \
+      -csv_file_name ${CSV_FILENAME} \
+      -test_csv_file_name ${TEST_CSV_FILENAME} \
+      -save_plots ${SAVE_PLOTS} \
+      &> ${OUTPUT_FILE}
     echo
     echo "Dumping ${OUTPUT_FILE} to stdout:"
     cat $OUTPUT_FILE
