33a2fdfd6b2fdf4a1d269f5f084a26aba5a01915
rudy jantz <porter@gatech.edu>
2021-01-08 12:46:23 -0500

    instr hpp

---
+++ src/passes/runtime/DebloatInstrument.hpp
@@ -0,0 +1,14 @@
+#ifndef WPD_DEBLOAT_INSTRUMENT_HPP
+#define WPD_DEBLOAT_INSTRUMENT_HPP
+
+#include <llvm/Analysis/GlobalsModRef.h>
+#include <llvm/Analysis/PostDominators.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Analysis/LoopInfo.h>
+
+using namespace llvm;
+using namespace std;
+
+#define DEBUG_TYPE "DebloatInstrument"
+
+#endif
