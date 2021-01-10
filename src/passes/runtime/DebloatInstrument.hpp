491d64b2c709346381a7ab92bec144498b3d74d2
rudy jantz <porter@gatech.edu>
2021-01-10 04:04:56 -0500

    add includes

--- src/passes/runtime/DebloatInstrument.hpp
+++ src/passes/runtime/DebloatInstrument.hpp
@@ -1,6 +1,11 @@
 #ifndef WPD_DEBLOAT_INSTRUMENT_HPP
 #define WPD_DEBLOAT_INSTRUMENT_HPP
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
 #include <llvm/Analysis/GlobalsModRef.h>
 #include <llvm/Analysis/PostDominators.h>
 #include <llvm/IR/IRBuilder.h>
