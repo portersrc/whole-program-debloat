2d5e93c85fed55fee9ce5f48f20963e33c6e965d
rudy jantz <porter@gatech.edu>
2021-01-09 20:47:38 -0500

    better folder management in make and run.sh

--- src/drivers/make.sh
+++ src/drivers/make.sh
@@ -4,17 +4,13 @@ set -euxo pipefail
 source env-vars
 
 
-rm -f $ALL_PASS_STATS
-
 for BMARK in "${BMARKS[@]}"; do
     echo "benchmark: $BMARK"
     FOLDER=${BMARK_TO_FOLDER[$BMARK]}
     echo "folder:    $FOLDER"
-    cd ${BASE_PATH}/${FOLDER}
-    make
+    make -C ${BASE_PATH}/${FOLDER}
   
-    echo $BMARK >> $ALL_PASS_STATS
-    cat debprof_pass_stats.txt
-    cat debprof_pass_stats.txt >> $ALL_PASS_STATS
-    echo "" >> $ALL_PASS_STATS
+    mkdir -p $BMARK
+    cat ${BASE_PATH}/${FOLDER}/debprof_pass_stats.txt >> $BMARK/pass_stats.txt
+    cat $BMARK/pass_stats.txt
 done
--- src/drivers/run.sh
+++ src/drivers/run.sh
@@ -8,7 +8,8 @@ for BMARK in "${BMARKS[@]}"; do
     echo "benchmark: $BMARK"
     FOLDER=${BMARK_TO_FOLDER[$BMARK]}
     echo "folder:    $FOLDER"
-    cd ${BASE_PATH}/${FOLDER}
+    pushd ${BASE_PATH}/${FOLDER}
     ./run.sh small
     ./run.sh medium
+    popd
 done
