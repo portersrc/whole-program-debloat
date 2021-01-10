40bfd072ffd7eca80bbe663d2db97202dc75a663
rudy jantz <porter@gatech.edu>
2021-01-10 04:01:13 -0500

    pass a func-set-filename to learn-dt.py

--- src/drivers/learn.sh
+++ src/drivers/learn.sh
@@ -9,6 +9,7 @@ for BMARK in "${BMARKS[@]}"; do
     FOLDER=${BMARK_TO_FOLDER[$BMARK]}
     echo "folder:    ${BASE_PATH}/${FOLDER}"
 
+    FUNC_SETS_FILENAME=${BASE_PATH}/${FOLDER}/debprof-func-set-ids-to-funcs.out
     CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-debprof.out
     TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-test-debprof.out
     #CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-debprof.outsmall
@@ -25,6 +26,7 @@ for BMARK in "${BMARKS[@]}"; do
     ${LEARN_PY} \
       -csv_file_name ${CSV_FILENAME} \
       -test_csv_file_name ${TEST_CSV_FILENAME} \
+      -func_sets_file_name ${FUNC_SETS_FILENAME} \
       -save_plots ${SAVE_PLOTS} \
       &> ${OUTPUT_FILE}
     echo
