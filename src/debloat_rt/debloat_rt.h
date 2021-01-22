c918477d8c9dd034fab6a447381c4d33f6357a7f
rudy jantz <porter@gatech.edu>
2021-01-22 02:51:54 -0500

    header file for debrt-return

--- src/debloat_rt/debloat_rt.h
+++ src/debloat_rt/debloat_rt.h
@@ -5,7 +5,11 @@ extern "C" {
 int debrt_monitor(int argc, ...);
 
 int debrt_protect(int argc, ...);
-int debrt_return(void);
+
+//int debrt_return(int);
+//int debrt_return(void *);
+int debrt_return(long long);
+
 int debrt_protect_loop(int argc, ...);
 int debrt_loop_end(void);
 
