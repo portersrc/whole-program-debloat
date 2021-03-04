73b7f1164163a319f5d878af55ce364a3cfc0dc3
rudy jantz <porter@gatech.edu>
2021-03-04 12:41:45 -0500

    parameterize post-process script

--- src/drivers/post-process.sh
+++ src/drivers/post-process.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -euxo pipefail
-
 source env-vars
 
 
-for BMARK in "${BMARKS[@]}"; do
+
+
+function post_process_bmark() {
+    BMARK=$1
     echo "benchmark: $BMARK"
     FOLDER=${BMARK_TO_FOLDER[$BMARK]}
     echo "folder:    ${BASE_PATH}/${FOLDER}"
     ${PARSE_DEBPROF_PY} ${BASE_PATH}/${FOLDER}
     #${PARSE_DEBPROF_PY} ${BASE_PATH}/${FOLDER} "process-test-data"
-done
+}
+
+
+
+
+if [ $# == 1 ]; then
+    echo "hit"
+    post_process_bmark $1
+else
+    for BMARK in "${BMARKS[@]}"; do
+        post_process_bmark $BMARK
+    done
+fi
