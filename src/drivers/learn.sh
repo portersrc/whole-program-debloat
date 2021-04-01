b990d73506290c7d373b534764f3caac51ccf5e3
rudy jantz <porter@gatech.edu>
2021-03-31 22:30:41 -0400

    update learn.sh to use cgpprof

--- src/drivers/learn.sh
+++ src/drivers/learn.sh
@@ -11,15 +11,19 @@ function learn_bmark() {
     FOLDER=${BMARK_TO_FOLDER[$BMARK]}
     echo "folder:    ${BASE_PATH}/${FOLDER}"
 
-    FUNC_SETS_FILENAME=${BASE_PATH}/${FOLDER}/debprof-func-set-ids-to-funcs.out
-    CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-debprof.out
-    TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-test-debprof.out
+    FUNC_SETS_FILENAME=${BASE_PATH}/${FOLDER}/cgpprof-func-set-ids-to-funcs.out
+    CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-cgpprof.out
+    #FUNC_SETS_FILENAME=${BASE_PATH}/${FOLDER}/debprof-func-set-ids-to-funcs.out
+    #CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-debprof.out
+    #TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-test-debprof.out
     #CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-train-debprof.outsmall
     #TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/final-test-debprof.outsmall
     #CSV_FILENAME=${BASE_PATH}/${FOLDER}/train-debprof.outsmall
     #TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/test-debprof.outsmall
     #CSV_FILENAME=${BASE_PATH}/${FOLDER}/train-debprof.outfull
     #TEST_CSV_FILENAME=${BASE_PATH}/${FOLDER}/test-debprof.outfull
+
+
     SAVE_PLOTS=${BMARK}-dt
     OUTPUT_FILE=${BMARK}-accuracy.txt
 
@@ -27,11 +31,11 @@ function learn_bmark() {
     pushd $BMARK &> /dev/null
     ${LEARN_PY} \
       -csv_file_name ${CSV_FILENAME} \
-      -test_csv_file_name ${TEST_CSV_FILENAME} \
       -func_sets_file_name ${FUNC_SETS_FILENAME} \
       -save_plots ${SAVE_PLOTS} \
       &> ${OUTPUT_FILE}
       #-do_accuracy True \
+      #-test_csv_file_name ${TEST_CSV_FILENAME} \
     echo
     echo "Dumping ${OUTPUT_FILE} to stdout:"
     cat $OUTPUT_FILE
