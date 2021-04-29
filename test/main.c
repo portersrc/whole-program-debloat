24f4cba3a5d0e42be66e3a690c3cfef5b80dd74b
rudy jantz <porter@gatech.edu>
2021-04-29 16:14:46 -0400

    test files

---
+++ test/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void E(void)
+{
+	printf("E\n");
+}
+void D(void)
+{
+	E();
+}
+void C(void)
+{
+	D();
+}
+void B(void)
+{
+	int i;
+	for(i = 0; i < 2; i++){
+		C();
+	}
+}
+
+void G(void)
+{
+	printf("G\n");
+}
+
+void F(void)
+{
+	int i;
+	for(i = 0; i < 2; i++)
+	{
+		G();
+	}
+	
+}
+void A(void)
+{
+	int i;
+	for(i = 0; i < 2; i++){
+		B();
+	}
+	F();
+}
+
+int main(void)
+{
+	A();
+	return 0;
+}
