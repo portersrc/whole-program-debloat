ed9be1721374e9f3bd4fa46732b070053e00286b
rudy jantz <porter@gatech.edu>
2021-10-01 16:58:55 -0400

    alternate technique for zeroing out parts of an ELF instead of glueing together pages; not needed for now and for ropper result it appears

---
+++ src/gadget-chains/0_makebins/zeroing-not-glueing/zero_out_file_part/zofp.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    void *null_bytes;
+
+    if(argc != 4){
+        printf("\n");
+        printf("Zero out part of a file.\n");
+        printf("\n");
+        printf("  Usage: \n");
+        printf("    ./zofp <file> <offset> <size>\n");
+        printf("\n");
+        exit(1);
+    }
+
+    char *lib = argv[1];
+    int offset = (int) strtol(argv[2], NULL, 16);
+    int size   = (int) strtol(argv[3], NULL, 16);
+
+    null_bytes = malloc(size);
+    memset(null_bytes, 0, size);
+
+    fp = fopen(lib, "r+b");
+    fseek(fp, offset, 0);
+    fwrite(null_bytes, size, 1, fp);
+    fclose(fp);
+
+    return 0;
+}
