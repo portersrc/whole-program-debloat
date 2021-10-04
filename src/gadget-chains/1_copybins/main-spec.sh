abf88b7967b5d507411455294711e05696732548
rudy jantz <porter@gatech.edu>
2021-10-04 19:55:18 -0400

    script for copybins for spec (actually does ln -s to help with space)

---
+++ src/gadget-chains/1_copybins/main-spec.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -euxo pipefail
+
+# cporter 2021.09.28
+# Copy the "pg" (page group) binaries for each benchmark into the "orig"
+# folder for rop-benchmark. This is essentially a staging area. Then execute
+# make. This will link the vuln.o against each pg bin file and produce
+# an executable for each one here:
+#   /home/rudy/wo/rop-benchmark/binaries/x86/reallife/vuln/debian-10-cloud
+#
+
+
+
+
+if [ $# == 1 ]; then
+    BMARKS=(
+        $1
+    )
+else
+    BMARKS=(
+        500.perlbench
+        510.parest
+        538.imagick
+        541.leela
+        519.lbm
+        505.mcf
+        520.omnetpp
+        523.xalancbmk
+        508.namd
+        511.povray
+        #526.blender
+        531.deepsjeng
+        544.nab
+        557.xz
+        525.x264
+    )
+fi
+
+
+
+
+for BMARK in "${BMARKS[@]}"; do
+
+    pushd /home/rudy/wo/rop-benchmark/binaries/x86/reallife/orig/debian-10-cloud
+    for F in `ls /home/rudy/wo/whole-program-debloat/src/gadget-chains/output/${BMARK}/*_pg_*.bin`; do
+        ln -s $F
+    done
+
+    pushd /home/rudy/wo/rop-benchmark/binaries/x86/reallife
+    make
+    popd
+
+    popd
+
+done
+echo "done"
