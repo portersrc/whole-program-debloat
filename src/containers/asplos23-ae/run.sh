c887f753d2564509fc9de7b0298c703a8ab3b94a
chris porter <porter@gatech.edu>
2022-10-07 09:20:26 -0400

    initial dockerfile and build and run scripts for artifact evaluation

---
+++ src/containers/asplos23-ae/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run \
+  -it \
+  --rm \
+  --network host \
+  -w /root/decker \
+  -e HOST_PERMS="$(id -u):$(id -g)" \
+  -v /home/rudy:/mnt \
+  --name decker-ae \
+  decker:latest
