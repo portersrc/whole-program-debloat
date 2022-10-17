1cb8cd5930c545fc9c14d88054a91339b6ef72bd
chris porter <porter@gatech.edu>
2022-10-17 14:39:47 -0400

    use a log and only return final result

--- src/gadget-spec/spec.sh
+++ src/gadget-spec/spec.sh
@@ -12,14 +12,15 @@ do
 
     start=$(readelf -S /root/decker/spec2017/benchspec/CPU/${bench}/build/build_peak_mytest-m64.0000/${bench}_wpd_custlink_ics | grep .text | awk '{print $4}')
     size=$(readelf -S /root/decker/spec2017/benchspec/CPU/${bench}/build/build_peak_mytest-m64.0000/${bench}_wpd_custlink_ics | grep .text -A 1 | awk '{getline; print $1}')
-    echo $bench
-    echo $start
-    echo $size
+    echo $bench &> spec.log
+    echo $start &>> spec.log
+    echo $size  &>> spec.log
     rm -rf resultswpd_cl_ics/$bench
-    python3 GSA.py --text_only --binary_version wpd_cl_ics --result_folder_name ${bench} /root/decker/spec2017/benchspec/CPU/$bench/build/build_peak_mytest-m64.0000/$bench"_wpd_custlink_ics" /root/decker/spec2017/benchspec/CPU/$bench/build/build_peak_mytest-m64.0000/debrt-mapped-rx-pages_wpd_cl_ics-security.out $start $size
+    python3 GSA.py --text_only --binary_version wpd_cl_ics --result_folder_name ${bench} /root/decker/spec2017/benchspec/CPU/$bench/build/build_peak_mytest-m64.0000/$bench"_wpd_custlink_ics" /root/decker/spec2017/benchspec/CPU/$bench/build/build_peak_mytest-m64.0000/debrt-mapped-rx-pages_wpd_cl_ics-security.out $start $size &>> spec.log
 
 done
 
-echo "Running calculate.py (which i hacked slightly so it had gcc)"
-python3 calculate.py --A --B --C --D --ics_linker
+echo "Running calculate.py (which i hacked slightly so it had gcc)" &>> spec.log
+python3 calculate.py --A --B --C --D --ics_linker &>> spec.log
 
+tail -n16 ics_linker.csv | awk -F',' '{print $1" "$4" "$6" "$8 }'
