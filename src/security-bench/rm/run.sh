1393513e97784e292d733a7d028e2cc11753afff
chris porter <porter@gatech.edu>
2022-10-17 17:03:37 -0400

    re-enable test cases for rm

--- src/security-bench/rm/run.sh
+++ src/security-bench/rm/run.sh
@@ -151,31 +151,31 @@ else
 fi
 
 
-#touch file1
-#{ time ./${BIN} file*; } &> 1-${WHICH}.out
-#rm -rf file*
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_1_$1.out
-#cp debrt.out debrt_1_$1.out
-#
-#touch .file1
-#{ time ./${BIN} .file1; } &> 2-${WHICH}.out
-#rm -rf file*
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_2_$1.out
-#cp debrt.out debrt_2_$1.out
-#
-#touch file file1 file2 file3
-#{ time ./${BIN} file1 file2 file3; } &> 3-${WHICH}.out
-#rm -rf file*
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_3_$1.out
-#cp debrt.out debrt_3_$1.out
-#
-#touch file file1 file2 file3
-#{ time ./${BIN} file*; } &> 4-${WHICH}.out
-#rm -rf file*
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_4_$1.out
-#cp debrt.out debrt_4_$1.out
-#
-#mkdir -p d1/d2
+touch file1
+{ time ./${BIN} file*; } &> 1-${WHICH}.out
+rm -rf file*
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_1_$1.out
+cp debrt.out debrt_1_$1.out
+
+touch .file1
+{ time ./${BIN} .file1; } &> 2-${WHICH}.out
+rm -rf file*
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_2_$1.out
+cp debrt.out debrt_2_$1.out
+
+touch file file1 file2 file3
+{ time ./${BIN} file1 file2 file3; } &> 3-${WHICH}.out
+rm -rf file*
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_3_$1.out
+cp debrt.out debrt_3_$1.out
+
+touch file file1 file2 file3
+{ time ./${BIN} file*; } &> 4-${WHICH}.out
+rm -rf file*
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_4_$1.out
+cp debrt.out debrt_4_$1.out
+
+mkdir -p d1/d2
 # 2021.09.24 cporter: This test returns 1.
 #{ time ./${BIN} d1; } &> 5-${WHICH}.out
 #cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_5_$1.out
@@ -184,69 +184,69 @@ fi
 #{ time ./${BIN} d1/d2; } &> 6-${WHICH}.out
 #cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_6_$1.out
 #cp debrt.out debrt_6_$1.out
-#{ time ./${BIN} -rf d1; } &> 7-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_7_$1.out
-#cp debrt.out debrt_7_$1.out
-#rm -rf d1
-#
-#mkdir -p d1/d2
-#mkdir -p d1/d3
-#{ time ./${BIN} -rf d1; } &> 8-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_8_$1.out
-#cp debrt.out debrt_8_$1.out
-#rm -rf d1
-#
-#mkdir -p d1/d2
-#mkdir -p d1/d3
-#{ time ./${BIN} -rf d1/*; } &> 9-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_9_$1.out
-#cp debrt.out debrt_9_$1.out
-#{ time ./${BIN} -rf d1; } &> 10-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_10_$1.out
-#cp debrt.out debrt_10_$1.out
-#rm -rf d1
-#
-#mkdir d1
-#touch d1/file1
-#touch d1/file2
-#touch d1/file3
-#{ time ./${BIN} d1/file*; } &> 11-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_11_$1.out
-#cp debrt.out debrt_11_$1.out
-#{ time ./${BIN} -rf d1; } &> 12-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_12_$1.out
-#cp debrt.out debrt_12_$1.out
-#rm -rf d1
-#
-#mkdir -p d1/d2/d3
-#touch d1/file1
-#touch d1/d2/file2
-#touch d1/d2/d3/file3
-#{ time ./${BIN} d1/file*; } &> 13-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_13_$1.out
-#cp debrt.out debrt_13_$1.out
-#{ time ./${BIN} d1/d2/file*; } &> 14-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_14_$1.out
-#cp debrt.out debrt_14_$1.out
-#{ time ./${BIN} d1/d2/d3/file*; } &> 15-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_15_$1.out
-#cp debrt.out debrt_15_$1.out
-#{ time ./${BIN} -rf d1; } &> 16-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_16_$1.out
-#cp debrt.out debrt_16_$1.out
-#rm -rf d1
-#
-#mkdir -p d1/d2/d3
-#touch d1/file1
-#touch d1/d2/file2
-#touch d1/d2/d3/file3
-#{ time ./${BIN} -rf d1; } &> 17-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_17_$1.out
-#cp debrt.out debrt_17_$1.out
-#rm -rf d1
-#
-#mkdir d1
-#touch d1/file d1/file2
+{ time ./${BIN} -rf d1; } &> 7-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_7_$1.out
+cp debrt.out debrt_7_$1.out
+rm -rf d1
+
+mkdir -p d1/d2
+mkdir -p d1/d3
+{ time ./${BIN} -rf d1; } &> 8-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_8_$1.out
+cp debrt.out debrt_8_$1.out
+rm -rf d1
+
+mkdir -p d1/d2
+mkdir -p d1/d3
+{ time ./${BIN} -rf d1/*; } &> 9-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_9_$1.out
+cp debrt.out debrt_9_$1.out
+{ time ./${BIN} -rf d1; } &> 10-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_10_$1.out
+cp debrt.out debrt_10_$1.out
+rm -rf d1
+
+mkdir d1
+touch d1/file1
+touch d1/file2
+touch d1/file3
+{ time ./${BIN} d1/file*; } &> 11-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_11_$1.out
+cp debrt.out debrt_11_$1.out
+{ time ./${BIN} -rf d1; } &> 12-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_12_$1.out
+cp debrt.out debrt_12_$1.out
+rm -rf d1
+
+mkdir -p d1/d2/d3
+touch d1/file1
+touch d1/d2/file2
+touch d1/d2/d3/file3
+{ time ./${BIN} d1/file*; } &> 13-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_13_$1.out
+cp debrt.out debrt_13_$1.out
+{ time ./${BIN} d1/d2/file*; } &> 14-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_14_$1.out
+cp debrt.out debrt_14_$1.out
+{ time ./${BIN} d1/d2/d3/file*; } &> 15-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_15_$1.out
+cp debrt.out debrt_15_$1.out
+{ time ./${BIN} -rf d1; } &> 16-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_16_$1.out
+cp debrt.out debrt_16_$1.out
+rm -rf d1
+
+mkdir -p d1/d2/d3
+touch d1/file1
+touch d1/d2/file2
+touch d1/d2/d3/file3
+{ time ./${BIN} -rf d1; } &> 17-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_17_$1.out
+cp debrt.out debrt_17_$1.out
+rm -rf d1
+
+mkdir d1
+touch d1/file d1/file2
 # 2021.09.24 cporter: This test uses -i, which "prompts before every removal".
 # In other words, need to press Enter twice when it runs.
 #{ time ./${BIN} -i d1/file*; } &> 18-${WHICH}.out
@@ -254,13 +254,13 @@ fi
 #cp debrt.out debrt_18_$1.out
 #rm -rf d1
 
-#mkdir d1
-#touch d1/file d1/file2
+mkdir -p d1
+touch d1/file d1/file2
 # 2021.09.24 cporter: This test returns 1.
 #{ time ./${BIN} -i d1; } &> 19-${WHICH}.out
 #cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_19_$1.out
 #cp debrt.out debrt_19_$1.out
-#{ time ./${BIN} -rf d1; } &> 20-${WHICH}.out
-#cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_20_$1.out
-#cp debrt.out debrt_20_$1.out
-#rm -rf d1
+{ time ./${BIN} -rf d1; } &> 20-${WHICH}.out
+cp debrt-mapped-rx-pages.out debrt-mapped-rx-pages_20_$1.out
+cp debrt.out debrt_20_$1.out
+rm -rf d1
