825006d8ff95ce1f6bb69fbd038f95116f1d724d
chris porter <porter@gatech.edu>
2022-10-18 13:13:21 -0400

    run-results support for nginx

--- src/nginx/performance/size-variation/run_results.sh
+++ src/nginx/performance/size-variation/run_results.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
-RESULTS_BASE=/home/rudy/wo/whole-program-debloat/results/2021-10-10/nginx/performance/size-variation
+#RESULTS_BASE=/home/rudy/wo/whole-program-debloat/results/2021-10-10/nginx/performance/size-variation
 
 function get_avg() {
     BASELINE_WPD=$1
     MB=$2
     SUM=0
-    for i in {1..3}; do
-        F=$i/wrk.${MB}mb.${BASELINE_WPD}.out
+    #for i in {1..3}; do
+        #F=$i/wrk.${MB}mb.${BASELINE_WPD}.out
+        F=wrk.${MB}mb.${BASELINE_WPD}.out
         TRANSFER_PER_SEC=$(grep "Transfer/sec" $F | awk '{print $2}')
         TRANSFER_PER_SEC=${TRANSFER_PER_SEC::-2}
         SUM=`echo $SUM + $TRANSFER_PER_SEC | bc`
-    done
+    #done
     AVG=`echo $SUM / 3.0 | bc -l`
     echo $AVG
 }
 
-pushd ${RESULTS_BASE}
+#pushd ${RESULTS_BASE}
 
 for MB in {1,10,100}; do
 
@@ -25,9 +26,9 @@ for MB in {1,10,100}; do
     WPD_AVG=`get_avg "wpd_custlink_ics" $MB`
     # slowdown is baseline / wpd, because we're looking at transfers/sec
     SLOWDOWN=`echo $BASELINE_AVG / $WPD_AVG | bc -l`
-    echo "avg SLOWDOWN for ${MB}mb: $SLOWDOWN"
+    echo "${MB}mb $SLOWDOWN"
 
 done
 
-popd
+#popd
 
--- src/nginx/performance/time-variation/run_results.sh
+++ src/nginx/performance/time-variation/run_results.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
-RESULTS_BASE=/home/rudy/wo/whole-program-debloat/results/2021-10-10/nginx/performance/time-variation
+#RESULTS_BASE=/home/rudy/wo/whole-program-debloat/results/2021-10-10/nginx/performance/time-variation
 
 function get_avg() {
     BASELINE_WPD=$1
     S=$2
     SUM=0
-    for i in {1..3}; do
-        F=$i/wrk.${S}s.${BASELINE_WPD}.out
+    #for i in {1..3}; do
+        #F=$i/wrk.${S}s.${BASELINE_WPD}.out
+        F=wrk.${S}s.${BASELINE_WPD}.out
         TRANSFER_PER_SEC=$(grep "Transfer/sec" $F | awk '{print $2}')
         TRANSFER_PER_SEC=${TRANSFER_PER_SEC::-2}
         SUM=`echo $SUM + $TRANSFER_PER_SEC | bc`
-    done
+    #done
     AVG=`echo $SUM / 3.0 | bc -l`
     echo $AVG
 }
 
-pushd ${RESULTS_BASE}
+#pushd ${RESULTS_BASE}
 
 for S in {3,30,300}; do
 
@@ -25,9 +26,9 @@ for S in {3,30,300}; do
     WPD_AVG=`get_avg "wpd_custlink_ics" $S`
     # slowdown is baseline / wpd, because we're looking at transfers/sec
     SLOWDOWN=`echo $BASELINE_AVG / $WPD_AVG | bc -l`
-    echo "avg SLOWDOWN for ${S}s: $SLOWDOWN"
+    echo "${S}s $SLOWDOWN"
 
 done
 
-popd
+#popd
 
