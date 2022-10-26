702ef193cef6b3353d118fc0f3ab2b2e34dd34a2
chris porter <porter@gatech.edu>
2022-10-26 18:20:24 -0400

    enable ics always in debrt

--- src/debloat_rt/debloat_rt.cpp
+++ src/debloat_rt/debloat_rt.cpp
@@ -37,12 +37,14 @@ int ENV_DEBRT_ENABLE_STATS = 0;
 // to enable, set env var DEBRT_ENABLE_STACK_CLEANING=1
 int ENV_DEBRT_ENABLE_STACK_CLEANING = 0;
 // to enable, set env var DEBRT_ENABLE_INDIRECT_CALL_SINKING=1
-int ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING = 0;
+//int ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING = 0;
 // to enable, set env var DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS=1
 int ENV_DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS = 0;
 // to enable, set env var DEBRT_ENABLE_TRACING=1
 //   Will attempt to trace all function calls (with the help of wpd pass)
 int ENV_DEBRT_ENABLE_TRACING = 0;
+
+#define DEBRT_INDIRECT_CALL_SINKING 1
 
 
 #define CGPredict
@@ -504,7 +506,7 @@ int update_page_counts(int func_id, int addend)
             _remap_permissions(addr, 1, RX_PERM);
             DEBRT_PRINTF("done RX\n");
 
-            if(ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING
+            if(DEBRT_INDIRECT_CALL_SINKING
             || ENV_DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS){
                 if( (addr >= text_start_aligned)
                 &&  (addr <=  text_end_aligned))
@@ -536,7 +538,7 @@ int update_page_counts(int func_id, int addend)
                 DEBRT_PRINTF("addr is above text end or part of last page. " \
                              "possibly part of .fini. ignoring mapping RO\n");
             }else{
-                if(ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING
+                if(DEBRT_INDIRECT_CALL_SINKING
                 || ENV_DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS){
                     _remap_permissions(addr, 1, RO_PERM);
                     //_remap_permissions(addr, 1, RX_PERM);
@@ -1935,9 +1937,9 @@ int debrt_init(int main_func_id, int sink_is_enabled)
     if(getenv("DEBRT_ENABLE_STACK_CLEANING")){
         ENV_DEBRT_ENABLE_STACK_CLEANING = 1;
     }
-    if(getenv("DEBRT_ENABLE_INDIRECT_CALL_SINKING")){
-        ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING = 1;
-    }
+    //if(getenv("DEBRT_ENABLE_INDIRECT_CALL_SINKING")){
+    //    ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING = 1;
+    //}
     if(getenv("DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS")){
         ENV_DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS = 1;
     }
@@ -1946,7 +1948,7 @@ int debrt_init(int main_func_id, int sink_is_enabled)
     }
     DEBRT_PRINTF("ENV_DEBRT_ENABLE_STATS: %d\n", ENV_DEBRT_ENABLE_STATS);
     DEBRT_PRINTF("ENV_DEBRT_ENABLE_STACK_CLEANING: %d\n", ENV_DEBRT_ENABLE_STACK_CLEANING);
-    DEBRT_PRINTF("ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING: %d\n", ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING);
+    //DEBRT_PRINTF("ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING: %d\n", ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING);
     DEBRT_PRINTF("ENV_DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS: %d\n", ENV_DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS);
     output_filename = getenv("DEBRT_OUT");
     if(!output_filename){
@@ -2003,7 +2005,7 @@ int debrt_init(int main_func_id, int sink_is_enabled)
     DEBRT_PRINTF("&func_name_to_id: 0x%p\n", &func_name_to_id);
     DEBRT_PRINTF("&func_name_to_id['main']: %p\n", &func_name_to_id["main"]);
     _debrt_protect_all_pages(RO_PERM);
-    if( (ENV_DEBRT_ENABLE_INDIRECT_CALL_SINKING == 0)
+    if( (DEBRT_INDIRECT_CALL_SINKING == 0)
     &&  (ENV_DEBRT_ENABLE_BASIC_INDIRECT_CALL_STATIC_ANALYSIS == 0) ){
         _debrt_map_ptd_to_funcs();
     }
