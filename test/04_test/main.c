f0fdf4d02e5fd81ec74dbb24f11ddd5e7e8871e3
chris porter <porter@gatech.edu>
2022-11-03 09:49:28 -0400

    test04

---
+++ test/04_test/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void B(char *s, int two, int three);
+
+
+void C(char *s, int four, int five, int six)
+{
+    printf("Args to C are:\n");
+    printf("  %s\n", s);
+    printf("  %d\n", four);
+    printf("  %d\n", five);
+    printf("  %d\n", six);
+    B("str arg to B (from C)", 22, 33);
+}
+
+void B(char *s, int two, int three)
+{
+    printf("B\n");
+    int i;
+    for(i = 0; i < 2; i++){
+        printf("  B loop iteration %d\n", i);
+    }
+}
+
+void A(char *s, int one, int two, int three, four)
+{
+    printf("Args to A are:\n");
+    printf("  %s\n", s);
+    printf("  %d\n", one);
+    int i;
+    for(i = 0; i < 2; i++){
+        B("str arg to B", 2, 3);
+    }
+    C("str arg to C", 4, 5, 6);
+}
+
+int main(void)
+{
+    A("str arg to A", 11, 101, 102, 105);
+    return 0;
+}
