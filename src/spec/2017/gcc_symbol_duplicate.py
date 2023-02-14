141ac43256fdfd201029a5ea9d7a7e92eb389bb1
chris porter <porter@gatech.edu>
2023-02-13 20:31:26 -0500

    add sharjeel's gcc script hack; note i put in the line='' thing that i think is the quickfix from the artifact reviews

---
+++ src/spec/2017/gcc_symbol_duplicate.py
@@ -0,0 +1,52 @@
+from os import listdir
+from os.path import isfile, join
+from shutil import copyfile
+
+onlyfiles = [f for f in listdir(".") if isfile(f) and ".bc" in f]
+onlyfiles.remove("main.bc")
+remove = ["@getchar()", "@vprintf(i8*", "@fgetc_unlocked(%struct._IO_FILE*", "@getc_unlocked(%struct._IO_FILE*", "@getchar_unlocked()", "@putchar(i32", "@fputc_unlocked(i32", 
+"@putc_unlocked(i32", "@putchar_unlocked(i32", "@getline(i8**", "@feof_unlocked(%struct._IO_FILE*", "@ferror_unlocked(%struct._IO_FILE*", "@tolower(i32", "@toupper(i32", 
+"@gnu_dev_major(i64", "@gnu_dev_minor(i64", "@gnu_dev_makedev(i32", "@atoi(i8*", "@atol(i8*", "@atoll(i8*", "@bsearch(i8*", "@atof(i8*", "@strtoimax(i8*", "@strtoumax(i8*", 
+"@wcstoimax(i32*", "@wcstoumax(i32*", "@stat(i8*", "@lstat(i8*", "@fstat(i32", "@fstatat(i32", "@mknod(i8*", "@mknodat(i32", "@stat64(i8*", "@lstat64(i8*", "@fstat64(i32", "@fstatat64(i32"]
+
+#onlyfiles = ["main.bc"]
+for f in onlyfiles:
+    string = ""
+    with open(f, 'r') as read:
+        try:
+            line = read.readline()
+        except UnicodeDecodeError:
+            # Quick hack: on the first build of gcc, this exception won't
+            # happen.
+            # On subsequence builds, however, there are proper .bc files (not
+            # these .ll files masquerading as .bc files), and they will
+            # throw an error when trying to read a line. Example of a file
+            # that would cause this: 502.gcc_r_linked_ics.bc. Just skip
+            # over them in case we ever need to.
+            line = ""
+        while(line):
+            temp = ""
+            temp += line
+            line = line.strip()
+            lst = line.split()
+            if(len(lst) >= 2 and lst[1] == "Function"):
+                line = read.readline()
+                temp += line
+                line = line.strip()
+                lst = line.split()
+                if(lst[2] in remove):
+                    temp = ""
+                    line = read.readline()
+                    while(line):
+                        lst = line.strip().split()
+                        if(len(lst) >= 1 and lst[0] == "}"):
+                            break
+                        line = read.readline()
+            string += temp
+                    
+            line = read.readline()
+
+    with open("sharjeel.txt", 'w') as write:
+        write.write(string)
+    
+    copyfile("sharjeel.txt",f)
