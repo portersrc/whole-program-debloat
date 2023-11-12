af5fb93af85da70c070accae60d38bcf106ccffb
chris porter <porter@gatech.edu>
2023-11-11 19:55:59 -0500

    add 08 testing files

---
+++ test/08_test/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euox pipefail
+
+function usage_exit() {
+    echo
+    echo "Usage: ./run.sh <which-binary>"
+    echo
+    exit 1
+}
+
+
+if [ $# != 1 ]; then
+    usage_exit
+fi
+
+WHICH=$1
+
+BIN=08.flagboy_${WHICH}
+
+source ${PROJ_DIR}/src/spec/2017/run_aux_preprocess.sh
+
+
+{ time ./${BIN}; } &> ${WHICH}.out
+
+source ${PROJ_DIR}/src/spec/2017/run_aux_postprocess.sh
