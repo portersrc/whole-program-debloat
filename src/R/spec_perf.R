13645787e5861a95199917656fbb6d925679433b
Chris Porter <porter@ibm.com>
2024-10-21 23:46:57 -0400

    latest spec-perf R file was never checked in

--- src/R/spec_perf.R
+++ src/R/spec_perf.R
@@ -5,8 +5,7 @@ library(ggplot2)
 
 
 df <- data.frame(
-  # legend=rep(c("Baseline", "Decker"), each=16),
-  legend=rep(c("Decker"), each=16),
+  legend=rep(c("ARTD"), each=16),
   bmark=rep(c(
     "perlbench", 
     "gcc",
@@ -28,41 +27,23 @@ df <- data.frame(
   ), 1),
   slowdown=
     c(
-      # # base_ls
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-      # 1,
-
-      # wpd_cl_ics (todo wpd_cl_ics_sc)
-      0.9903420105, # perlbench
-      1.143244326, # gcc
-      1.011573185, # mcf
-      0.9990883179, # namd
-      1.024487023, # parest
-      1.071922054, # povray
-      1.036504238, # lbm
-      1.117012653, # omnetpp
-      1.085095903, # xalancbmk
-      1.029968998, # x264
-      1.000281204, # blender
-      1.003534296, # deepsjeng
-      0.9827746151, # imagick
-      1.024797731, #leela
-      1.02610419, # nab
-      1.051924182 # xz
+      # artd_release
+      1.151149972,
+      1.255446818,
+      1.089307389,
+      0.9725246969,
+      0.9901314488,
+      1.150307284,
+      1.016685525,
+      1.223465965,
+      1.156748286,
+      1.121568822,
+      1.040528503,
+      0.9899329116,
+      1.042722421,
+      0.9955339443,
+      0.990014574,
+      1.018175077
   )
 )
 
@@ -89,7 +70,7 @@ ggplot(data=df, aes(x=bmark, y=slowdown, fill=legend)) +
   theme(axis.text.y=element_text(size = 15)) +
   theme(axis.text.x=element_text(angle = -45, hjust = 0, size = 15)) +
   #ylim(0.9, 2)
-  coord_cartesian(ylim = c(0.95, 1.15)) +
+  coord_cartesian(ylim = c(0.95, 1.30)) +
   theme(legend.title = element_blank()) +
   theme(legend.position = c(.9,.8)) +
   theme(axis.title.x=element_blank()) +
